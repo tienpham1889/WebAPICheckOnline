7dbf329531a44d85d15936c7684bed9e7771521e
tienpham1889 <55777923+tienpham1889@users.noreply.github.com>
2020-07-24 00:17:26 +0700

    update monhoc theo lop hoc phan

--- WebAPI_KTOnline/Models/SinhVien.cs
+++ WebAPI_KTOnline/Models/SinhVien.cs
@@ -66,8 +66,7 @@ namespace WebAPI_KTOnline.Models
         {
             SinhVien sv = new SinhVien();
 
-            string ChuoiKetNoi = @"Data Source=MR-TIEN\SQLEXPRESS;Initial Catalog =DBCheckOnline;Integrated Security = True";
-            SqlConnection conn = new SqlConnection(ChuoiKetNoi);
+            SqlConnection conn = DataProvider.Connect();
             conn.Open();
             string sQuery = string.Format("select * from SinhVien where MaSV = '{0}'", masv);
             SqlCommand comm = new SqlCommand(sQuery, conn);
