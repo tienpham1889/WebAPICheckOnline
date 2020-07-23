98b39cf1c1eaf030f934b05d86661374f573b8aa
Mac Dinh Thanh <55576731+MacDinhThanh@users.noreply.github.com>
2020-07-23 10:45:28 +0700

    test

--- WebAPI_KTOnline/Models/DataProvider.cs
+++ WebAPI_KTOnline/Models/DataProvider.cs
@@ -11,7 +11,7 @@ namespace WebAPI_KTOnline.Models
     {
         public static SqlConnection Connect()
         {
-            string ChuoiKetNoi = @"Data Source=MR-TIEN\SQLEXPRESS;Initial Catalog =DBCheckOnline;Integrated Security = True";
+            string ChuoiKetNoi = @"Data Source=DESKTOP-319D2UA\SQLEXPRESS;Initial Catalog =DBQSProject;Integrated Security = True";
             SqlConnection conn = new SqlConnection(ChuoiKetNoi);
             return conn;
         }
