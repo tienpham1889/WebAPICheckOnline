f3c493185605a5e524e6cfeac62b0ccd438d950c
tienpham1889 <55777923+tienpham1889@users.noreply.github.com>
2020-07-25 15:56:46 +0700

    update add classSelection

--- WebAPI_KTOnline/Controllers/LopHocPhanController.cs
+++ WebAPI_KTOnline/Controllers/LopHocPhanController.cs
@@ -43,8 +43,65 @@ namespace WebAPI_KTOnline.Controllers
         }
 
         // POST: api/LopHocPhan
-        public void Post([FromBody]string value)
+        public LopHocPhan Post([FromBody]LopHocPhan lopHP)
         {
+            LopHocPhan lhp = new LopHocPhan();
+            SqlConnection conn = DataProvider.Connect();
+            conn.Open();
+            string malhp = LopHocPhan.layLopHocPhan();
+            string mamh = "";
+            string magv = "";
+            string malop = "";
+            string sQuery_getMonHoc = string.Format("SELECT MaMonHoc FROM MonHoc WHERE TenMonHoc = N'{0}'", lopHP.maMonHoc);
+            string sQuery_getGiaoVien = string.Format("SELECT MaGV FROM GiangVien WHERE TenGV = N'{0}'", lopHP.maGiaoVien);
+            string sQuery_getLop = string.Format("SELECT MaLop FROM Lop WHERE TenLop = N'{0}'", lopHP.maLop);
+            SqlCommand com1 = new SqlCommand(sQuery_getMonHoc, conn);
+            SqlCommand com2 = new SqlCommand(sQuery_getGiaoVien, conn);
+            SqlCommand com3 = new SqlCommand(sQuery_getLop, conn);
+            SqlDataReader dr = com1.ExecuteReader();
+            while (dr.Read())
+            {
+                mamh = dr.GetString(0);
+            }
+            dr.Close();
+            SqlDataReader dr2 = com2.ExecuteReader();
+            while (dr2.Read())
+            {
+                magv = dr2.GetString(0);
+            }
+            dr2.Close();
+            SqlDataReader dr3 = com3.ExecuteReader();
+            while (dr3.Read())
+            {
+                malop = dr3.GetString(0);
+            }
+            dr3.Close();
+            if (lhp.kiemtra(lopHP.tenLopHocPhan))
+            {
+
+                String sQuery = "INSERT INTO [dbo].[LopHocPhan]([MaLopHP],[TenLopHP],[MaGV],[MaMonHoc],[MaLop],[TrangThai])VALUES(@malophp,@tenlophp,@magv,@mamonhoc,@malop,@trangthai)";
+                SqlCommand insert_LHPcommand = new SqlCommand(sQuery, conn);
+                insert_LHPcommand.Parameters.AddWithValue("@malophp", malhp);
+                insert_LHPcommand.Parameters.AddWithValue("@tenlophp", lopHP.tenLopHocPhan);
+                insert_LHPcommand.Parameters.AddWithValue("@magv", magv);
+                insert_LHPcommand.Parameters.AddWithValue("@mamonhoc", mamh);
+                insert_LHPcommand.Parameters.AddWithValue("@malop", malop);
+                insert_LHPcommand.Parameters.AddWithValue("@trangthai", lopHP.trangThai);
+                int result = insert_LHPcommand.ExecuteNonQuery();
+
+                lhp = lhp.lhp(malhp);
+                if (result > 0)
+                {
+                    return lhp;
+                }
+            }
+
+            else
+            {
+                return lhp;
+            }
+            conn.Close();
+            return lhp;
         }
 
         // PUT: api/LopHocPhan/5
--- WebAPI_KTOnline/Models/LopHocPhan.cs
+++ WebAPI_KTOnline/Models/LopHocPhan.cs
@@ -32,7 +32,6 @@ namespace WebAPI_KTOnline.Models
         public string maGiaoVien { get => Magv; set => Magv = value; }
         public string maMonHoc { get => Mamonhoc; set => Mamonhoc = value; }
         public string maLop { get => Malop; set => Malop = value; }
-        //s
         public int trangThai { get => Trangthai; set => Trangthai = value; }
         public static List<LopHocPhan> DsachLop()
         {
@@ -61,6 +60,28 @@ namespace WebAPI_KTOnline.Models
             conn.Close();
             return list;
         }
+        public LopHocPhan lhp(string malhp)
+        {
+            LopHocPhan lophp = new LopHocPhan();
+
+            SqlConnection conn = DataProvider.Connect();
+            conn.Open();
+            string sQuery = string.Format("select * from LopHocPhan where MaLopHP = '{0}'", malhp);
+            SqlCommand comm = new SqlCommand(sQuery, conn);
+            SqlDataReader dr = comm.ExecuteReader();
+            while (dr.Read())
+            {
+                lophp.MalopHP = dr.GetString(0);
+                lophp.TenlopHP = dr.GetString(1);
+                lophp.Magv = dr.GetString(2);
+                lophp.Mamonhoc = dr.GetString(3);
+                lophp.Malop = dr.GetString(4);
+                lophp.Trangthai = dr.GetInt32(5);
+            }
+            dr.Close();
+            conn.Close();
+            return lophp;
+        }
         public static List<LopHocPhan> DsachLHP_Theomonhoc(string mamh)
         {
             List<LopHocPhan> list = new List<LopHocPhan>();
@@ -82,6 +103,51 @@ namespace WebAPI_KTOnline.Models
             }
             conn.Close();
             return list;
+        }
+        public bool kiemtra(string tenlophp)
+        {
+            SqlConnection conn = DataProvider.Connect();
+            conn.Open();
+            string sQuery = string.Format("select * from LopHocPhan where TenLopHP = N'{0}' ", tenlophp);
+            SqlCommand comm = new SqlCommand(sQuery, conn);
+            SqlDataReader dr = comm.ExecuteReader();
+            int count = 0;
+            while (dr.Read())
+            {
+                count++;
+
+            }
+            if (count > 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static string layLopHocPhan()
+        {
+            SqlConnection conn = DataProvider.Connect();
+            conn.Open();
+            string mavd = "";
+            string sQuery1 = "select top 1 MaLopHP from LopHocPhan order by MaLopHP desc";
+            SqlCommand com1 = new SqlCommand(sQuery1, conn);
+            SqlDataReader dr1 = com1.ExecuteReader();
+            while (dr1.Read())
+            {
+                mavd = dr1.GetString(0);
+            }
+            dr1.Close();
+            conn.Close();
+            string lucsau = mavd.Substring(3);
+            int sott = Convert.ToInt32(lucsau);
+            int stt_tieptheo = sott + 1;
+            string matieptheo = "LHP";
+            string masott = stt_tieptheo.ToString();
+            while (masott.Length < 6)
+            {
+                masott = "0" + masott;
+            }
+            matieptheo += masott;
+            return matieptheo;
         }
     }
 }
