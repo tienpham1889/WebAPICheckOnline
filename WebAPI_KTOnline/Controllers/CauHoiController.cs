b9adbbaeb511aac08af0fc8753af50f7b18ebe6f
tienpham1889 <55777923+tienpham1889@users.noreply.github.com>
2020-07-29 00:15:21 +0700

    update theo ma class cau hoi

--- WebAPI_KTOnline/Controllers/CauHoiController.cs
+++ WebAPI_KTOnline/Controllers/CauHoiController.cs
@@ -21,26 +21,9 @@ namespace WebAPI_KTOnline.Controllers
         }
         [HttpGet]
         [Route("api/Cauhoi-theomonhoc-chude")]
-        public IEnumerable<CauHoi> Get_cauhoitheodieukien(string tenmonhoc, string tenchude)
+        public IEnumerable<CauHoi> Get_cauhoitheodieukien(string mamonhoc, string machude)
         {
-            string mamonhoc = "";
-            string machude = "";
-            SqlConnection conn = DataProvider.Connect();
-            conn.Open();
-            StringBuilder sQuery = new StringBuilder();
-            sQuery.Append("select  CD.MaMonHoc, CD.MaCD ");
-            sQuery.Append("FROM ChuDe CD ");
-            sQuery.Append("inner join MonHoc MH on CD.MaMonHoc = MH.MaMonHoc ");
-            sQuery.AppendFormat("WHERE MH.TenMonHoc = N'{0}' AND CD.TenCD = N'{1}'", tenmonhoc,tenchude);
-            SqlCommand com = new SqlCommand(sQuery.ToString(), conn);
-            SqlDataReader dr = com.ExecuteReader();
-            while (dr.Read())
-            {
-                mamonhoc = dr.GetString(0);
-                machude = dr.GetString(1);
-            }
-            conn.Close();
-            List<CauHoi> Dsachcauhoi_monhoc_chude = CauHoi.DsachCauhoi_theodieukien(mamonhoc,machude);
+            List<CauHoi> Dsachcauhoi_monhoc_chude = CauHoi.DsachCauhoi_theodieukien(mamonhoc, machude);
             return Dsachcauhoi_monhoc_chude;
         }
         // GET: api/CauHoi/5
@@ -56,26 +39,8 @@ namespace WebAPI_KTOnline.Controllers
             SqlConnection conn = DataProvider.Connect();
             conn.Open();
             string mach = CauHoi.layMaCauHoi();
-            string mamh = "";
-            string macd = "";
             int trangthai = 1;
-            string sQuery_getMonHoc = string.Format("SELECT MaMonHoc FROM MonHoc WHERE TenMonHoc = N'{0}'", cauhoi.maMonHoc);
-            string sQuery_getChuDe = string.Format("SELECT MaCD FROM ChuDe WHERE TenCD = N'{0}'", cauhoi.maChuDe);
-            SqlCommand com = new SqlCommand(sQuery_getMonHoc, conn);
-            SqlCommand com2 = new SqlCommand(sQuery_getChuDe, conn);
-            SqlDataReader dr = com.ExecuteReader();
-            while (dr.Read())
-            {
-                mamh = dr.GetString(0);
-            }
-            dr.Close();
-            SqlDataReader dr2 = com2.ExecuteReader();
-            while (dr2.Read())
-            {
-                macd = dr2.GetString(0);
-            }
-            dr2.Close();
-            if (ch.kiemtra(cauhoi.noiDung, mamh))
+            if (ch.kiemtra(cauhoi.noiDung, cauhoi.maMonHoc))
             {
 
                 String sQuery = "INSERT INTO [dbo].[CauHoi]([MaCauHoi],[NoiDung],[PhuongAnA],[PhuongAnB],[PhuongAnC],[PhuongAnD],[DapAn],[MaCD],[TrangThai],[MaMonHoc])VALUES(@mach,@noidung,@phuongana,@phuonganb,@phuonganc,@phuongand,@dapan,@macd,@trangthai,@mamonhoc)";
@@ -87,9 +52,9 @@ namespace WebAPI_KTOnline.Controllers
                 insert_CauHoicommand.Parameters.AddWithValue("@phuonganc", cauhoi.phuongAnC);
                 insert_CauHoicommand.Parameters.AddWithValue("@phuongand", cauhoi.phuongAnD);
                 insert_CauHoicommand.Parameters.AddWithValue("@dapan", cauhoi.dapAn);
-                insert_CauHoicommand.Parameters.AddWithValue("@macd", macd);
+                insert_CauHoicommand.Parameters.AddWithValue("@macd", cauhoi.maChuDe);
                 insert_CauHoicommand.Parameters.AddWithValue("@trangthai", trangthai);
-                insert_CauHoicommand.Parameters.AddWithValue("@mamonhoc", mamh);
+                insert_CauHoicommand.Parameters.AddWithValue("@mamonhoc", cauhoi.maMonHoc);
                 int result = insert_CauHoicommand.ExecuteNonQuery();
 
                 ch = ch.cauhoi(mach);
