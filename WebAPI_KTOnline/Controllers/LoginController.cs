bba8c6924b195ade53d3a6f14db283b76ba95560
tienpham1889 <55777923+tienpham1889@users.noreply.github.com>
2020-07-31 00:58:45 +0700

    update moi

--- WebAPI_KTOnline/Controllers/LoginController.cs
+++ WebAPI_KTOnline/Controllers/LoginController.cs
@@ -30,7 +30,7 @@ namespace WebAPI_KTOnline.Controllers
         {
             SqlConnection conn = DataProvider.Connect();
             conn.Open();
-            String sQuery = string.Format("SELECT * FROM SinhVien where MaSV = '{0}' and Passsword = '{1}'", sv.maSinhVien, sv.matKhau);
+            String sQuery = string.Format("SELECT * FROM SinhVien where MaSV = '{0}' and Passsword = '{1}'", sv.maSinhVien, StringProc.MD5Hash(sv.matKhau));
             SqlCommand comm = new SqlCommand(sQuery, conn);
             int count = 0;
             SqlDataReader dr = comm.ExecuteReader();
@@ -57,7 +57,7 @@ namespace WebAPI_KTOnline.Controllers
         {
             SqlConnection conn = DataProvider.Connect();
             conn.Open();
-            String sQuery = string.Format("SELECT * FROM GiangVien where MaGV = '{0}' and Passsword = '{1}'", gv.maGiaoVien, gv.matKhau);
+            String sQuery = string.Format("SELECT * FROM GiangVien where MaGV = '{0}' and Passsword = '{1}'", gv.maGiaoVien, StringProc.MD5Hash(gv.matKhau));
             SqlCommand comm = new SqlCommand(sQuery, conn);
             int count = 0;
             SqlDataReader dr = comm.ExecuteReader();
